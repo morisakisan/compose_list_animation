a9de5a2a080bc6b45c8b7cabc0e3a96fb7d697b3
shxun6934 <b1612851@planet.kanazawa-it.ac.jp>
2022-08-26 18:54:45 +0900

    アニメーションの方法検討

--- app/src/main/java/com/example/testpaging/MainActivity.kt
+++ app/src/main/java/com/example/testpaging/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,160 +12,128 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.lifecycleScope
-import androidx.paging.*
-import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.items
-import com.example.testpaging.data.Favorite
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import com.example.testpaging.data.Sample
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    private val queryFlow = MutableStateFlow(true)
+    private var _data = MutableStateFlow(Sample.lists)
+    private val data: StateFlow<List<Sample>> = _data.asStateFlow()
 
-    private class SamplePagingSource(private val query: Boolean) : PagingSource<Int, String>() {
+    private val animateMethod: AnimateMethod = AnimateMethod.ANIMATED_CONTENT
 
-        companion object {
-            const val MAX_PAGE = 10
-        }
-
-        override fun getRefreshKey(state: PagingState<Int, String>): Int? {
-            return state.anchorPosition
-        }
-
-        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, String> {
-            val perPage = params.loadSize
-            val page = params.key ?: 0
-            val next = if (page + 1 > MAX_PAGE) null else page + 1
-            val prev = params.key?.let { it - 1 }
-
-            return LoadResult.Page(
-                data = (0..perPage).map { "query = $query:$page-$it-DATA" },
-                prevKey = prev,
-                nextKey = next
-            )
-        }
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    private val samplePagingFlow = queryFlow.flatMapLatest { query ->
-        Pager(
-            PagingConfig(
-                initialLoadSize = 20,
-                pageSize = 20
-            )
-        ) {
-            SamplePagingSource(query)
-        }.flow
-    }.cachedIn(lifecycleScope)
-
-    private var _data = MutableStateFlow(Favorite.lists)
-    private val data: StateFlow<List<Favorite>> = _data.asStateFlow()
-
-    // Paging使用フラグ
-    private val paging: Boolean = false
-
-    @OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            if (paging) {
-                val lazyPagingItems = samplePagingFlow.collectAsLazyPagingItems()
+            val list by data.collectAsState()
 
-                Scaffold(
-                    floatingActionButton = {
-                        Button(onClick = {
-                            lifecycleScope.launch {
-                                queryFlow.emit(!queryFlow.value)
-                            }
-                        }) {
-                            Text(text = "おしてね")
-                        }
-                    }
-                ) { padding ->
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(padding)
+            when (animateMethod) {
+                AnimateMethod.ANIMATED_VISIBILITY -> AnimatedVisibility(list)
+                AnimateMethod.ANIMATED_CONTENT -> AnimatedContent(list)
+                AnimateMethod.ANIMATE_ITEM_PLACEMENT -> AnimateItemPlacement(list)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun AnimatedVisibility(list: List<Sample>) {
+        Scaffold { padding ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                items(list, key = { it.id }) { item ->
+                    var visible by rememberSaveable { mutableStateOf(true) }
+
+                    AnimatedVisibility(
+                        modifier = Modifier.animateItemPlacement(),
+                        visible = visible,
+                        enter = fadeIn(),
+                        exit = fadeOut(),
                     ) {
-                        items(lazyPagingItems, key = { it }) { sample ->
-                            var visible by rememberSaveable { mutableStateOf(true) }
-
-                            AnimatedVisibility(
-                                modifier = Modifier.animateItemPlacement(),
-                                visible = visible,
-                                enter = fadeIn(),
-                                exit = fadeOut(),
-                            ) {
-                                Card(
-                                    modifier = Modifier
-                                        .padding(10.dp)
-                                        .clickable {
-                                            visible = !visible
-                                        }
-                                ) {
-                                    Text(
-                                        text = sample ?: "",
-                                        modifier = Modifier
-                                            .height(100.dp)
-                                            .fillMaxWidth(),
-                                    )
-                                }
-                            }
-                        }
-                    }
-                }
-            } else {
-                val list by data.collectAsState()
-
-                Scaffold { padding ->
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(padding)
-                    ) {
-                        items(list) { item ->
-                            AnimatedContent(
-                                targetState = item,
-                                modifier = Modifier.animateItemPlacement(),
-                                transitionSpec = {
-                                    (slideInVertically { height -> height } + fadeIn() with slideOutVertically { height -> -height } + fadeOut())
-                                        .using(SizeTransform(clip = false))
-                                }
-                            ) { favorite ->
-                                Text(
-                                    text = favorite.name,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .background(LightGray.copy(alpha = 0.2f))
-                                        .padding(20.dp)
-                                        .clickable {
-                                            _data.value = data.value - favorite
-                                        }
-                                )
-                            }
+                        CardItem(item = item) {
+                            visible = !visible
                         }
                     }
                 }
             }
         }
+    }
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Composable
+    fun AnimatedContent(list: List<Sample>) {
+        Scaffold { padding ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                items(list) { item ->
+                    AnimatedContent(
+                        targetState = item,
+                        transitionSpec = {
+                            slideInVertically { height -> height } + fadeIn() with slideOutVertically { height -> -height } + fadeOut()
+                        }
+                    ) { sample ->
+                        CardItem(item = item) {
+                            _data.value = data.value - sample
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun AnimateItemPlacement(list: List<Sample>) {
+        Scaffold { padding ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                items(list, key = { it.id }) { item ->
+                    CardItem(modifier = Modifier.animateItemPlacement(), item = item) {
+                        _data.value = data.value - item
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun CardItem(modifier: Modifier = Modifier, item: Sample, onClick: () -> Unit) {
+        Card(
+            modifier = modifier
+                .padding(10.dp)
+                .clickable(onClick = onClick)
+        ) {
+            Text(
+                text = item.name,
+                modifier = Modifier
+                    .height(100.dp)
+                    .fillMaxWidth()
+            )
+        }
+    }
+
+    enum class AnimateMethod {
+        ANIMATED_VISIBILITY,
+        ANIMATED_CONTENT,
+        ANIMATE_ITEM_PLACEMENT
     }
 }
